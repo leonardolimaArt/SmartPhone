51d9aaa069dc774b7248a9cbd2c234489a2ea283
Leonardo Lima <54562792+leonardolimaArt@users.noreply.github.com>
2023-03-31 09:38:27 -0400

    Delete Smartphone directory

--- Smartphone/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Smartphone.Model;
-
-Nokia nokia1 = new Nokia("9999-9999", "X9-01", "123456789", "128Mb");
-nokia1.InstalarAplicativo("Calculadora");
-
-IPhone iPhone = new IPhone("8888-8888", "iPhone 14", "987654321", "128Gb");
-iPhone.InstalarAplicativo("WhatsApp");
